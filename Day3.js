aa0d32e3f2f39fded38eb16efbd10cb4a71f1687
Thomas de Groot <degroot12@gmail.com>
2020-12-09 15:01:50 +0100

    werkt alleen met volledige input, geen idee hoe het moet met dupliceren van input

---
+++ Day3.js
@@ -0,0 +1,19 @@
+const fs = require('fs');
+
+const trees = fs.readFileSync('Day3.txt', 'utf8');
+
+
+const splitLines = trees.split('\n');
+
+let count = 0;
+
+splitLines.forEach((row, index) => {
+    const splitTree = row.split('');
+    const moveRight = splitTree[index + index + index]
+    const moveMoreRight = index + index + index
+
+    if(moveRight === '#') {
+        count ++;
+    }
+})
+console.log(count);
